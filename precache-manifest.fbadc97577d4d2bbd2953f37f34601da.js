75eb495b6e92ce00ca6b342fec0496f21ec93e69
Denis Demyanok <DenisRusis@ex.ua>
2021-10-07 23:30:06 +0300

    Updates

---
+++ precache-manifest.fbadc97577d4d2bbd2953f37f34601da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4efe12aac38dbd2ce22ede70b93b5c2e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "47221cf387f0b7228528",
+    "url": "./static/css/main.ae4f840b.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47221cf387f0b7228528",
+    "url": "./static/js/main.d173d316.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
